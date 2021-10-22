7bc1a4cb6099b93438018a3ec117b021ccea9899
Shahboz007 <olmasovshahboz007@gmail.com>
2021-10-22 19:59:41 +0500

    Updates

---
+++ precache-manifest.6eb56a228f9c8e97cef73d3797dc8506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0a2cbebce71746b89332d4a1a36071",
+    "url": "/accordion_question/index.html"
+  },
+  {
+    "revision": "e2b84b79d3acf55310aa",
+    "url": "/accordion_question/static/css/main.08744408.chunk.css"
+  },
+  {
+    "revision": "562fb19fe3d711f428ca",
+    "url": "/accordion_question/static/js/2.077b7fb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion_question/static/js/2.077b7fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b84b79d3acf55310aa",
+    "url": "/accordion_question/static/js/main.375e4c53.chunk.js"
+  },
+  {
+    "revision": "485cccf1f38a31e4d679",
+    "url": "/accordion_question/static/js/runtime-main.73ce1b22.js"
+  }
+]);
